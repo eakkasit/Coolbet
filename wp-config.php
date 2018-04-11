75faf4a29a178cb107164c3650801a9db1f3c37b
eakkasit <eakkasitk@gmail.com>
2018-04-11 08:16:26 +0000

    merge master file

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'densistebookmaker');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '9ho1717!');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
