899de8e15dd60e3e98bd8e1323906d450f94a124
Thanadech Pokinwattanakul <cpooup@gmail.com>
2018-04-11 13:16:53 +0700

    modify title bar

---
+++ wp-content/themes/understrap-child/header.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package understrap
+ */
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<div class="hfeed site" id="page">
+
+
+<div class="header">
+	<div class="title-head">
+		<div class="container">
+			<div class="row">
+				<div class="title-logo col-md-3">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+				<?php 
+					if ( has_custom_logo() ) {
+							the_custom_logo();
+					} 
+				?><!-- end custom logo -->
+				</div>
+				<div class="title-name col-md-6">
+					<!-- <?php if ( is_front_page() && is_home() ) : ?>
+
+					<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
+
+					<?php else : ?>
+
+					<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+
+					<?php endif; ?> -->
+					<img class="img-fluid" src="<?php echo includes_url(); ?>/images/logo_text.png" >
+				</div>
+				<div class="title-social col-md-3">
+					<img class="img-fluid" src="<?php echo includes_url(); ?>/images/logo_facebook.png">
+					<img class="img-fluid" src="<?php echo includes_url(); ?>/images/logo_twister.png" >
+					<img class="img-fluid" src="<?php echo includes_url(); ?>/images/logo-linked.png" >
+					<img class="img-fluid" src="<?php echo includes_url(); ?>/images/logo_instragram.png" >
+				</div>
+			</div>		
+		</div>		
+	</div>				
+	<div class="title-menu">
+		<div class="container">
+			<div class="row">
+			<?php wp_nav_menu(
+				array(
+					'theme_location'  => 'primary',
+					'container_class' => '',
+					'container_id'    => '',
+					'menu_class'      => '',
+					'fallback_cb'     => '',
+					'menu_id'         => 'main-menu',
+					'walker'          => new understrap_WP_Bootstrap_Navwalker(),
+				)
+			); ?>		
+			</div>		
+		</div>
+	</div>
+</div>
