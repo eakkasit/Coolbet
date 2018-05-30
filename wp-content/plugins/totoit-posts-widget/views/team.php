a908e3da51a92d66d9a76d08e8f40d92815ed980
BeasZ <eakkasitk@gmail.com>
2018-05-30 14:50:59 +0700

    update reamore button in bookmakerne

--- wp-content/plugins/totoit-posts-widget/views/team.php
+++ wp-content/plugins/totoit-posts-widget/views/team.php
@@ -44,7 +44,7 @@ if( $totoit_posts->have_posts() ):
 						<h5 class="pt-3"><?php the_title(); ?></h5>
 						<h3><?php echo get_field( "job_title", get_the_ID() );?></h3>
 						<div class="content-video-detail text-center">
-							<a class="btn read-more" href="<?php echo get_permalink( $id ) ?>">Read More</a>
+							<a class="btn btn-secondary read-more" href="<?php echo get_permalink( $id ) ?>">Read More</a>
 						</div>
 						<!-- <p class="m-0 p-0"><?php //echo wp_trim_words( get_the_content(), 15, '...' ); ?></p> -->
 						</div>
